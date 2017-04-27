f04ee39d4f17fb14454136931532edc9bc90eeca
matheuscfernandes <matheuscfernandes@gmail.com>
2017-04-27 15:44:55 -0400

    progress on code

--- p4/code/stub.py
+++ p4/code/stub.py
@@ -10,13 +10,16 @@ class Learner(object):
     '''
 
     def __init__(self,Space_Discretization,Eps,Gamma,Eta):
-        self.last_state  = None
-        self.last_action = None
-        self.last_reward = None
+        self.reset()
         self.Space_Discretization=Space_Discretization
+        self.velocity_segments=10
+        self.max_V=40
         self.Eps=Eps
         self.Gamma=Gamma
         self.Eta=Eta
+        self.screen_width=600
+        self.screen_height=400
+        self.Q=np.zeros((2,self.velocity_segments+1,int(self.screen_width/self.Space_Discretization)+1,int(self.screen_height/self.Space_Discretization)+1))
 
     def reset(self):
         self.last_state  = None
@@ -29,70 +32,56 @@ class Learner(object):
         Implement this function to learn things and take actions.
         Return 0 if you don't want to jump and 1 if you do.
         '''
+
+        self.state=state
+        Space_Discretization=self.Space_Discretization
+
+
         def random_act(): #Generating random action for exploration
         	return npr.choice([0,1])
-        def MeasureGravity(self):
-            abs(self.state['monkey']['vel']-self.last_state['monkey']['vel'])
-            
-
-
+        def MeasureGravity(self,state,last_state):
+            return abs(state['monkey']['vel']-last_state['monkey']['vel'])
+        def VelocityNorm(self,v):
+            Segs=self.velocity_segments
+            Max_V=self.max_V
+            V_norm=int((float(v)/float(Max_V)+1.)*Segs/2.)
+            if V_norm>Segs: V_norm=Segs
+            if V_norm<0: V_norm=0
+            return V_norm
 
         ### ---- Obtaining Current State Parameters ---- ####
-        #TREE PARAMETERS
-        TreeDist=state['tree']['dist']/self.Space_Discretization
-        TreeTopDist=state['tree']['top']
-        TreeBotDist=state['tree']['bot']
-        TreeDist=state['tree']['dist']
+        Dist=int(state['tree']['dist']/Space_Discretization)
+        Height=int((state['monkey']['bot']-state['tree']['bot'])/Space_Discretization)
+        Vel=VelocityNorm(state['monkey']['vel'])
 
-        #MONKEY PARAMETERS
-        MonkV=state['monkey']['vel']
-        MonkHeight=state['monkey']['bot']
-
-        if self.last_action!=None:
-            ### ---- Obtaining Previous State Parameters ---- ####
-            LastTreeDist=last_state['tree']['dist']/self.Space_Discretization
-            LastTreeTopDist=last_state['tree']['top']
-            LastTreeBotDist=last_state['tree']['bot']
-            LastTreeDist=last_state['tree']['dist']
-
-            #MONKEY PARAMETERS
-            LastMonkV=last_state['monkey']['vel']
-            LastMonkHeight=last_state['monkey']['bot']
-
-            if self.gravity==None:
-                Grav=MeasureGravity()
-                if Grav<0: self.gravity=abs(Grav)
-            
-
-
-
-        else: new_action=random_act()
-
-
-
-
-
-
-        if npr.rand()<self.eta: 
-        	new_action=random_act()
-        	
+        if self.last_action==None: new_action=0
         else:
-            #initialize the parameters
 
+                last_state=self.last_state
+
+                ### ---- Obtaining Previous State Parameters ---- ####
+                LastDist=int(last_state['tree']['dist']/Space_Discretization)
+                LastHeight=int((last_state['monkey']['bot']-last_state['tree']['bot'])/Space_Discretization)
+                LastVel=VelocityNorm(last_state['monkey']['vel'])
+
+                if self.gravity==None:
+                    Grav=MeasureGravity(state,last_state)
+                    if Grav<0: self.gravity=int(abs(Grav))
+
+                if npr.rand()<self.eta: new_action=random_act()
+                
+                else:    
+
+                new_action=
+
+                self.Q[self.last_action]
 
 
         
-
-
-
-       	else:
-
-
-        new_state  = state
-
+        	
 
         self.last_action = new_action
-        self.last_state  = new_state
+        self.last_state  = self.state
 
         print state
         print new_action
@@ -102,7 +91,6 @@ class Learner(object):
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
-        print reward
         self.last_reward = reward
 
 
