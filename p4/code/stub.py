f0bf6cd8885bf4e7cae41be3828ccb17cb48c81d
matheuscfernandes <matheuscfernandes@gmail.com>
2017-04-28 10:19:52 -0400

    added printing function for quantification to stub.

--- p4/code/stub.py
+++ p4/code/stub.py
@@ -106,7 +106,7 @@ def run_games(learner, hist, iters = 100, t_len = 100):
     for ii in range(iters):
         # Make a new monkey object.
         swing = SwingyMonkey(sound=False,                  # Don't play sounds.
-                             text="Epoch %d" % (ii),       # Display the epoch on screen.
+                             text="Epoch %d \\ Heighest Score %d" % (ii,best_score),       # Display the epoch on screen.
                              tick_length = t_len,          # Make game ticks super fast.
                              action_callback=learner.action_callback,
                              reward_callback=learner.reward_callback)
